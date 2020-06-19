eb9e673b9f96712eceb06f7fd627917f87c174ad
melisandre <melisandre.schofield@gmail.com>
2020-06-19 12:34:50 -0400

    formatting

--- infos/27.php
+++ infos/27.php
@@ -1,20 +1,28 @@
-	<div class=“blurb”>
-		Walls. We built our research with as large a scope as possible, as many researchers as we could manage with absolutely no funding. Embracing cross-disciplinary approaches and scientific agnosticism, this four-year expedition ties together anthropology, biology, dream research, travel, art, thought experiments, and glue-slathered popsicle sticks.
+	<button type="button" class="collapsible">about</button>
+	<div class=“content”>
+		<p>Walls. We built our research with as large a scope as possible, as many researchers as we could manage with absolutely no funding. Embracing cross-disciplinary approaches and scientific agnosticism, this four-year expedition ties together anthropology, biology, dream research, travel, art, thought experiments, and glue-slathered popsicle sticks.</p>
 	</div>
 
-	<div class=“statement”>
+	<button type="button" class="collapsible">researcher statement</button>
+	<div class=“content”>
 		<h4>researcher statement</h4>
-		In pulling together the team, Robert and I placed a special attention on differences in research approach. We considered the theme as a metaphor that could inform our collaborative practices. 
+		<p>In pulling together the team, Robert and I placed a special attention on differences in research approach. We considered the theme as a metaphor that could inform our collaborative practices.</p>
 	</div>	
-	<div class="archivist statement">
-		<h4>archivist statement</h4>
-		Although we're able to confirm that this research gave rise to a number of strange wall-like structures in and around the Boston area, where M. Kozlowski lives and works, we have not been able to acquire fuller documentation of research activities and notes pertinent to this multi-faceted work. We have come to believe that the archives are most likely being stored in the garage of the grandparents of one of the unnamed grad students who worked on this project in late 2014. 
-	</div>
-	<div class="bio">
-		<h4>Fred Morse</h4>
-			After a brief stint at the University of Rotterdam, where he did a post-doc in biometrics, Fred Morse got an adjunct position at a Wisconsin State College, and taught introductory classes while he earned a Masters of Computational Arts from Something Academy, combining his love of scientific illustrations with cross-disciplinary research practices.   
-		<h4>Robert Kozlowski</h4>
-			Amateur Architect and Engineer, Robert Kozlowski has a degree in Law and the mind of a conspiracy theorist. He participated in this project, urged by long-time friend and confidant Fred Morse. The extent of his involvement is debatable. 
 
+	<button type="button" class="collapsible">archivist statement</button>
+	<div class="content">
+		<h4>archivist statement</h4>
+		<p>Although we're able to confirm that this research gave rise to a number of strange wall-like structures in and around the Boston area, where M. Kozlowski lives and works, we have not been able to acquire fuller documentation of research activities and notes pertinent to this multi-faceted work. We have come to believe that the archives are most likely being stored in the garage of the grandparents of one of the unnamed grad students who worked on this project in late 2014.</p>
 	</div>
+
+	<button type="button" class="collapsible">bios</button>
+	<div class="content">
+		<h4>Fred Morse</h4>
+			<p>After a brief stint at the University of Rotterdam, where he did a post-doc in biometrics, Fred Morse got an adjunct position at a Wisconsin State College, and taught introductory classes while he earned a Masters of Computational Arts from Something Academy, combining his love of scientific illustrations with cross-disciplinary research practices.</p>  
+		<h4>Robert Kozlowski</h4>
+			<p>Amateur Architect and Engineer, Robert Kozlowski has a degree in Law and the mind of a conspiracy theorist. He participated in this project, urged by long-time friend and confidant Fred Morse. The extent of his involvement is debatable.</p>
+		<h4>Anonymous Underlings</h4>
+			<p>A number of citizen scientists, artists, and grad students worked tirelessly to make this research possible.</p>			
+	</div>
+
 
