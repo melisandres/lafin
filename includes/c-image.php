c84d53edfb4bcee5bbbbe6d2af78083457140246
melisandre <melisandre.schofield@gmail.com>
2020-06-29 19:10:42 -0400

    image tooltip shows alt text. toolTip added to json

--- includes/c-image.php
+++ includes/c-image.php
@@ -1,7 +1,7 @@
 <?php 
 
 function displayImage($onD){
-    echo "<figure class='center-image'> <div class='cell'> <img src=".$onD[image1]." alt='".$onD[alt1]."' title='test title'> </div> <figcaption> ".$onD[caption1]." </figcaption> </figure>";
+    echo "<figure class='center-image'> <div class='cell'> <img src=".$onD[image1]." alt='".$onD[alt1]."' title='".$onD[alt1]."'> </div> <figcaption> ".$onD[caption1]." </figcaption> </figure>";
 }
 displayImage($onDisplay);
 
