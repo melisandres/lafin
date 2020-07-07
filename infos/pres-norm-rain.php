e5b1c6a96bf3350fe863a376283ba493840e99f2
melisandre <melisandre.schofield@gmail.com>
2020-07-07 11:36:52 -0400

    a new archive

---
+++ infos/pres-norm-rain.php
@@ -0,0 +1,30 @@
+<div class="archive-c-text">	
+	<button type="button" class="collapsible">about</button>
+	<div class="content">
+	<p>In this era of doomsday meteorology, emerges a strange subgroup of citizen scientists mesuring rain, not with modern day tools and technologies, not even with pre-modern instruments. This is a careful and long-term study by those who use just their bodies, and sometimes a mug full of coffee, to delve into the phenomenological study of the thining line between rain and hail.</p>
+	</div>
+
+	<button type="button" class="collapsible">statement</button>
+	<div class="content">
+		<h4>statement title</h4>
+		<p>statement description.</p>
+	</div>	
+
+	<button type="button" class="collapsible">notes</button>
+	<div class="content">
+		<h4>recovered notes:</h4>
+		<p>Notes description</p>
+	</div>
+
+	<button type="button" class="collapsible">other</button>
+	<div class="content">
+		<h4>other title</h4>
+		<p>other description.</p>
+	</div>
+
+	<button type="button" class="collapsible">bio</button>
+	<div class="content">
+		<h4>Name</h4>
+		<p>bio</p>  		
+	</div>
+</div>
