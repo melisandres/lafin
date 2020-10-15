6c05817b77252d2ea129ecbc5e22050e509b6ad5
melisandre <melisandre.schofield@gmail.com>
2020-10-15 11:39:05 -0400

    history page: join us section: content edit

--- history.php
+++ history.php
@@ -56,24 +56,29 @@
 		with Lafin to further the mission. 
 	</p>
 	<p>
-		In writing our history, we observe the way moments 
-		stick to their neighbours, almost as rigidly as how 
-		every time-line stands apart from the natural flow of 
-		time, chopping the shapeless into pieces, pretending 
-		moments could properly be represented by nodes on a 
-		two-dimentional surface squished into 
-		quasi-unidimentionality. Time travel is one way to 
-		describe the activity of archiving. It is something we
-		approach with some degree of trepidation&mdash;but if 
-		that's the only way to set a meeting, we'll oblige. 
+		In writing our history, we observe the ways in which 
+		moments stick to their neighbours, with a kind of glue 
+		that gets stuck to our archivists' fingers as they 
+		pull the moments apart. We also observe the unique way
+		in which every time-line stands apart from the natural 
+		flow of time, chopping the shapeless into pieces, 
+		convinced of the notion that moments can properly be 
+		represented by nodes on a two-dimentional surface 
+		squished into quasi-unidimentionality.
 	</p>
 	<p> 
-		At Lafin, the end in french, we consider the meeting of
-		ends, the strands of time. On the page, we try to smooth
-		over the gaps and the inconsistencies. In the world, 
-		we try to crate the moments that will mark the obvious
-		breaks, that might be welded together in their telling.
-		Join us. 
+		Time travel is one way to describe the activity of 
+		archiving. It is something we approach with some 
+		degree of trepidation&mdash;but since it's often
+		the only way to catalogue wayward archives, we often
+		oblige. 
+	</p>
+	<p> 
+		At Lafin we consider the meeting of ends, the strands 
+		of time. On the page, we try to smooth over the gaps 
+		and the inconsistencies. In the world, we try to crate 
+		the moments that will mark the obvious breaks, that 
+		might be welded together in their telling. Join us. 
 		Find out <a class="join-us-link" href="javascript:;">more</a>.
 	</p>
 	</div>
