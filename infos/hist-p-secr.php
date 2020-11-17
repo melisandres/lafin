792d72d46b4779f9575879a8bd609675931d113f
melisandre <melisandre.schofield@gmail.com>
2020-11-17 15:59:24 -0500

    content editing

--- infos/hist-p-secr.php
+++ infos/hist-p-secr.php
@@ -1,10 +1,11 @@
 <div class="archive-c-text">	
 	<button type="button" class="collapsible">about</button>
 	<div class="content">
-	<p>On the sidewalk across from the residency, on the subway towards the 
-	Education Wing, through the crack in the door where Stacey keeps the printing
-	press, in the empty café chair still warm from our gallerist, we write 
-	the back pages of Lafin's history.</p>
+	<p>On the shady patches of sidewalk across from the residency, on the long subway 
+	ride towards the Education Wing, through the orange-lit crack in the 
+	door behind which Stacey keeps the printing press, in the empty café 
+	chair still warm from our gallerist, we write the back pages of Lafin's
+	history.</p>
 	<p>At Lafin, we feel it is important to pierce small holes in the veil 
 	between the professional and the personal, allowing light to 
 	spill through. In the warm glow of pin-prick lights, we trace 
