18bf48ea7106394421368b1d36a970e989aa8545
melisandre <melisandre.schofield@gmail.com>
2021-01-05 09:53:55 -0500

    content : new archive

--- infos/hist-corr-findm.php
+++ infos/hist-corr-findm.php
@@ -1,30 +1,35 @@
 <div class="archive-c-text">	
 	<button type="button" class="collapsible">about</button>
 	<div class="content">
-	<p>about</p>
-	</div>
-
-	<button type="button" class="collapsible">transcript</button>
-	<div class="content">
-		<h4>The following was transcribed in 1977 using a decoder ring</h4>
-		<p>meow meow meow. Meow. Meow. Meow. MMMMMOOOEEEOOOWWW.</p>
+		<h4>1977</h4>
+		<p>In 1977, the board did not meet. James, in a desire 
+		to create a virtual meeting place, created a folder
+		that was sent to each board member. The folder included: 
+		instructions, a topographical map drawn on acetate, 
+		and an accompanying text, that read like a screenplay, 
+		and was meant to be torn, as per the surviving instructions. </p>
 	</div>	
 
-	<button type="button" class="collapsible">notes</button>
+	<button type="button" class="collapsible">instructions</button>
 	<div class="content">
-		<h4>recovered notes:</h4>
-		<p>Notes description</p>
+		<h4>recovered instructions:</h4>
+		<p>Once out of your front door, listen attentively. If you 
+		are not in anyone's way, close your eyes to listen even 
+		more carefully. Turn Right, with your eyes open, with the 
+		sounds etched in your consciousness.</p>
 	</div>
 
-	<button type="button" class="collapsible">other</button>
+	<button type="button" class="collapsible">archivist notes</button>
 	<div class="content">
-		<h4>other title</h4>
-		<p>other description.</p>
-	</div>
-
-	<button type="button" class="collapsible">bio</button>
-	<div class="content">
-		<h4>Name</h4>
-		<p>bio</p>  		
+		<h4>archivist notes</h4>
+		<p>We were able to find the instructions and the map. 
+		But, sadly, we were unable to find the accompanying text.
+		We chose to follow the instructions, and held the fragile 
+		acetate over a street map of our home. We therefor attended
+		the meeting virtually, more than 40 years later. We created 
+		a stand-in for the lost accompanying text: a script-like 
+		document translating an imaginary argument about the nature 
+		of time. We then invited today's board to join us in the 
+		ceremony. Most did.</p>
 	</div>
 </div>
