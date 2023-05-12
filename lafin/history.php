8d6a75f3a25dfc5cf97e61de588096bd5e710e93
melisandre <melisandre.schofield@gmail.com>
2023-05-11 22:40:50 -0400

    minor text edits

--- lafin/history.php
+++ lafin/history.php
@@ -63,29 +63,17 @@
 		with Lafin to further the mission. 
 	</p>
 	<p>
-		In writing our history, we observe the ways in which 
-		moments stick to their neighbours, with a kind of glue 
-		that gets stuck to our archivists' fingers as they 
-		pull the moments apart. We also observe the unique way
-		in which every time-line stands apart from the natural 
-		flow of time, chopping the shapeless into pieces, 
-		convinced of the notion that moments can properly be 
-		represented by nodes on a two-dimentional surface 
-		squished into quasi-unidimentionality.
+		In putting together our history, moments stick to other 
+		moments. A gluey substance transfers to everything our 
+		archivists' touch, as they engage in the strange task of 
+		pulling moments apart, and setting time-lines against
+		the natural flow of time, to dry them before their 
+		pressing.
 	</p>
 	<p> 
-		Time travel is one way to describe the activity of 
-		archiving. It is something we approach with some 
-		degree of trepidation&mdash;but since it's often
-		the only way to catalogue wayward archives, we often
-		oblige. 
-	</p>
-	<p> 
-		At Lafin we consider the meeting of ends, the strands 
-		of time. On the page, we try to smooth over the gaps 
-		and the inconsistencies. In the world, we try to crate 
-		the moments that will mark the obvious breaks, that 
-		might be welded together in their telling. Join us. 
+		At Lafin history is pressing. It is the past and the 
+		future all happening at once, discovery and invention
+		are one. Join us. 
 		Find out <a class="join-us-link" href="javascript:;">more</a>.
 	</p>
 	</div>
